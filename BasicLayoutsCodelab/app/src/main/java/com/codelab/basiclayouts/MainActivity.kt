f15924708a9c08616d0f7ec2bfd552e8c113e302
Animesh Roy <royanimesh2211@gmail.com>
2026-02-14 12:14:41 +0600

    Added scrolling behaviour

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -38,7 +39,9 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -160,6 +163,7 @@ fun AlignYourBodyRow(
     LazyRow(
         modifier = modifier,
         horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
     ) {
         items(alignYourBodyData) { item ->
             AlignYourBodyElement(drawable = item.drawable, text = item.text)
@@ -208,7 +212,22 @@ fun HomeSection(
 // Step: Home screen - Scrolling
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    // Implement composable here
+
+    Column(modifier
+        .verticalScroll(rememberScrollState())) {
+
+        Spacer(Modifier.height(16.dp))
+        SearchBar(modifier = Modifier.padding(horizontal = 16.dp))
+
+        HomeSection(R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(R.string.favorite_collections) {
+            FavoriteCollectionsGrid()
+        }
+        Spacer(Modifier.height(16.dp))
+
+    }
 }
 
 // Step: Bottom navigation - Material
@@ -240,17 +259,7 @@ fun MySootheAppLandscape() {
 fun MySootheApp() {
     // Implement composable here
 
-    Column {
-        SearchBar(modifier = Modifier.padding(8.dp))
-
-        HomeSection(R.string.align_your_body) {
-            AlignYourBodyRow()
-        }
-        HomeSection(R.string.favorite_collections) {
-            FavoriteCollectionsGrid()
-        }
-
-    }
+   HomeScreen()
 }
 
 private val alignYourBodyData = listOf(
